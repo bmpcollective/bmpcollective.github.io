01ce7a0f692a240dcd412dc4b2e493a91be93b4e
Barak Binaymin <barak.binyamin555@gmail.com>
2025-02-14 22:35:03 -0500

    fixed logo position

--- src/src/pages/home/index.vue
+++ src/src/pages/home/index.vue
@@ -13,34 +13,35 @@
 
 <style scoped>
 .home{
-    margin: 0;
-    width: 100%;
-    height: 100%;
-    min-height: 100%;
-    display: grid;
-    align-items: center;
-    align-content: center;
+    margin         : 0;
+    width          : 100%;
+    height         : 100%;
+    min-height     : 100%;
+    display        : grid;
+    align-items    : center;
+    align-content  : center;
     justify-content: center;
-    justify-items: center;
-    overflow: hidden;
+    justify-items  : center;
+    overflow       : hidden;
 }
 .hero-logo{
+    width    : 100%;
     max-width: 600px;
-    padding: 50px 50px 40px 50px;
+    padding  : 0px 50px 40px 50px;
 }
 .navbar{
-    display    : grid;
+    display              : grid;
     grid-template-columns:  min-content min-content min-content min-content min-content;
-    max-width  : 550px;
-    font-size  : 18px;
-    font-family: canada-type-gibson, Helvetica, sans-serif;
-    font-weight: 200;
-    color      : #131416;
-    grid-gap   : 10px;
-    overflow   : hidden;
+    max-width            : 550px;
+    font-size            : 18px;
+    font-family          : canada-type-gibson, Helvetica, sans-serif;
+    font-weight          : 200;
+    color                : #131416;
+    grid-gap             : 10px;
+    overflow             : hidden;
 }
 .dropdown-option{
-    padding: 5px;
-    text-transform: uppercase;
+    padding        : 5px;
+    text-transform : uppercase;
 }
 </style>
